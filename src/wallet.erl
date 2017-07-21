ec500260cb28fb95d36163c5cbf63a7762c8bc06
Maryam Dabaghchian <maryam81609@users.noreply.github.com>
2017-07-21 13:39:48 -0600

    API functions do not take the server node as input

--- src/wallet.erl
+++ src/wallet.erl
@@ -1,32 +1,39 @@
 -module(wallet).
 
 %% API
--export([credit/4, debit/4, transfer/5, get_val/3]).
+-export([credit/3, debit/3, transfer/4, get_val/2]).
 
 -include_lib("eunit/include/eunit.hrl").
 
-debit(Node, Wallet, Amount, ST) ->
-    {ok, Tx1} = rpc:call(Node, antidote, start_transaction, [ST, []]),
-    ok = rpc:call(Node, antidote, update_objects, [[{Wallet, decrement, Amount}], Tx1]),
-    {ok, [Res1]} = rpc:call(Node, antidote, read_objects, [[Wallet], Tx1]),
-    {ok, CT1} = rpc:call(Node, antidote, commit_transaction, [Tx1]),
+-define(DB_NODE, antidote_node).
+-define(APP, wallet).
+
+debit(Wallet, Amount, ST) ->
+    {ok, AntNode} = application:get_env(?APP, ?DB_NODE),
+    {ok, Tx1} = rpc:call(AntNode, antidote, start_transaction, [ST, []]),
+    ok = rpc:call(AntNode, antidote, update_objects, [[{Wallet, decrement, Amount}], Tx1]),
+    {ok, [Res1]} = rpc:call(AntNode, antidote, read_objects, [[Wallet], Tx1]),
+    {ok, CT1} = rpc:call(AntNode, antidote, commit_transaction, [Tx1]),
     {Res1, {Tx1, CT1}}.
 
-credit(Node, Wallet, Amount, ST) ->
-    {ok, Tx2} = rpc:call(Node, antidote, start_transaction, [ST, []]),
-    ok = rpc:call(Node, antidote, update_objects, [[{Wallet, increment, Amount}], Tx2]),
-    {ok, [Res2]} = rpc:call(Node, antidote, read_objects, [[Wallet], Tx2]),
-    {ok, CT2} = rpc:call(Node, antidote, commit_transaction, [Tx2]),
+credit(Wallet, Amount, ST) ->
+    {ok, AntNode} = application:get_env(?APP, ?DB_NODE),
+    {ok, Tx2} = rpc:call(AntNode, antidote, start_transaction, [ST, []]),
+    ok = rpc:call(AntNode, antidote, update_objects, [[{Wallet, increment, Amount}], Tx2]),
+    {ok, [Res2]} = rpc:call(AntNode, antidote, read_objects, [[Wallet], Tx2]),
+    {ok, CT2} = rpc:call(AntNode, antidote, commit_transaction, [Tx2]),
     {Res2, {Tx2, CT2}}.
 
-transfer(Node, FromWallet, ToWallet, Amount, ST) ->
-    {ok, TxTrnsfr} = rpc:call(Node, antidote, start_transaction, [ST, []]),
-    ok = rpc:call(Node, antidote, update_objects, [[{FromWallet, decrement, Amount}, {ToWallet, increment, Amount}], TxTrnsfr]),
-    {ok, CT} = rpc:call(Node, antidote, commit_transaction, [TxTrnsfr]),
+transfer(FromWallet, ToWallet, Amount, ST) ->
+    {ok, AntNode} = application:get_env(?APP, ?DB_NODE),
+    {ok, TxTrnsfr} = rpc:call(AntNode, antidote, start_transaction, [ST, []]),
+    ok = rpc:call(AntNode, antidote, update_objects, [[{FromWallet, decrement, Amount}, {ToWallet, increment, Amount}], TxTrnsfr]),
+    {ok, CT} = rpc:call(AntNode, antidote, commit_transaction, [TxTrnsfr]),
     CT.
 
-get_val(Node, Wallet, Clock) ->
-    {ok, Tx} = rpc:call(Node, antidote, start_transaction, [Clock, []]),
-    {ok, [Res]} = rpc:call(Node, antidote, read_objects, [[Wallet], Tx]),
-    {ok, _CT1} = rpc:call(Node, antidote, commit_transaction, [Tx]),
+get_val(Wallet, Clock) ->
+    {ok, AntNode} = application:get_env(?APP, ?DB_NODE),
+    {ok, Tx} = rpc:call(AntNode, antidote, start_transaction, [Clock, []]),
+    {ok, [Res]} = rpc:call(AntNode, antidote, read_objects, [[Wallet], Tx]),
+    {ok, _CT1} = rpc:call(AntNode, antidote, commit_transaction, [Tx]),
     Res.
